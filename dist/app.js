ca537242de52cae3810757b16fac2f50c4703997
Amhjadkhan <amjadpogramer@gmail.com>
2024-09-12 00:14:17 +0400

    this is api route setup commit

--- dist/app.js
+++ dist/app.js
@@ -7,13 +7,27 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const cors_1 = __importDefault(require("cors"));
 const product_route_1 = require("./app/modules/Products/product.route");
+// import { OrderRoutes } from "./app/modules/orders/order.route";
 const port = 3000;
 // parsers
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: [
+        'http://localhost:5173'
+    ],
+    credentials: true,
+}));
+const getController = (req, res) => {
+    res.send('this is nursary server ');
+};
+app.get('/', getController);
 // application routes
-app.use('/api', product_route_1.ProductRoutes);
-app.get("/", (req, res) => {
-    res.send("this is nursary server");
+app.use('/api/v4', product_route_1.ProductRoutes);
+// app.use('/api/v4', OrderRoutes);
+app.all('*', (req, res) => {
+    res.status(400).json({
+        success: false,
+        message: 'Route is not found!',
+    });
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,34 @@ import express, { Application, Request, Response } from "express";
 const app: Application = express();
 import cors from "cors";
 import { ProductRoutes } from "./app/modules/Products/product.route";
+// import { OrderRoutes } from "./app/modules/orders/order.route";
 const port = 3000;
 
 // parsers
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173'],
+    credentials: true,
+  }),
+);
 
+const getController = (req: Request, res: Response) => {
+  res.send('this is nursary server ');
+};
+app.get('/', getController);
 
 // application routes
-app.use('/api', ProductRoutes);
+app.use('/api/v4', ProductRoutes);
+// app.use('/api/v4', OrderRoutes);
 
-
-
-app.get("/", (req: Request, res: Response) => {
-    res.send("this is nursary server");
+app.all('*', (req: Request, res: Response) => {
+  res.status(400).json({
+    success: false,
+    message: 'Route is not found!',
   });
+});
 
 
   export default app;
